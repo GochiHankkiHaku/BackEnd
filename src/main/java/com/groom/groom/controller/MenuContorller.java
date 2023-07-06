393697504a7ebc56e43a1e48bc474adc9ee25f73
hyoung <ycat01@naver.com>
2023-07-07 07:38:05 +0900

    [FEAT] gpt 추가, 메뉴 리스트 all

--- src/main/java/com/groom/groom/controller/MenuContorller.java
+++ src/main/java/com/groom/groom/controller/MenuContorller.java
@@ -9,10 +9,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "menu", description = "메뉴")
 @RestController
@@ -24,7 +23,17 @@ public class MenuContorller {
     @Operation(summary = "menu/save", description = "메뉴추가")
     public ResponseEntity<Menu> save(@RequestBody MenuSaveDto menuSaveDto){
         Menu menu = menuService.saveMenu(menuSaveDto);
+
         return ResponseEntity.status(HttpStatus.CREATED).body(menu);
     }
+
+    @GetMapping("/listall")
+    @Operation(summary = "menu/listall", description = "메뉴 전체보기")
+    public ResponseEntity<List<Menu>> findAllPosts(){
+        List<Menu> list = menuService.findAll(); // 메뉴 전체를 조회하는 메소드 호출
+        return ResponseEntity.status(HttpStatus.OK).body(list); // 조회된 메뉴 리스트를 응답으로 반환
+    }
+
+
 
 }
